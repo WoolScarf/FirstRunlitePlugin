387592b3a32caadf89faf08ccc1bdfe1f3042306
Woolscarf <yanj465@gmail.com>
2023-07-25 01:27:51 +0300

    Initial changes

--- src/main/java/com/ReVerb/ReVerb.java
+++ src/main/java/com/ReVerb/ReVerb.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.ReVerb;
 
 import com.google.inject.Provides;
 import javax.inject.Inject;
@@ -14,15 +14,15 @@ import net.runelite.client.plugins.PluginDescriptor;
 
 @Slf4j
 @PluginDescriptor(
-	name = "Example"
+	name = "ReVerb"
 )
-public class ExamplePlugin extends Plugin
+public class ReVerb extends Plugin
 {
 	@Inject
 	private Client client;
 
 	@Inject
-	private ExampleConfig config;
+	private ReVerbConfig config;
 
 	@Override
 	protected void startUp() throws Exception
@@ -46,8 +46,8 @@ public class ExamplePlugin extends Plugin
 	}
 
 	@Provides
-	ExampleConfig provideConfig(ConfigManager configManager)
+	ReVerbConfig provideConfig(ConfigManager configManager)
 	{
-		return configManager.getConfig(ExampleConfig.class);
+		return configManager.getConfig(ReVerbConfig.class);
 	}
 }
